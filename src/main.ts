ea0155d47c2b55ca819f1514e8f40132bd828087
Hinoki <yukihinoki@qq.com>
2022-02-15 22:11:49 +0800

    feat: use fect-ui and pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// router
 import router from '@web/routes'
+// pinia
+import { createPinia } from 'pinia'
+// fect-ui
+import Fect from '@fect-ui/vue'
+import '@fect-ui/vue/lib/main.css'
 
 const app = createApp(App)
 app.use(router)
-
+app.use(createPinia())
+app.use(Fect)
 app.mount('#app')
