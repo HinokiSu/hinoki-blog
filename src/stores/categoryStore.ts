c8f92f45e8375b96062c5ef4aa68e00746766863
Hinoki <yukihinoki@qq.com>
2022-02-16 22:32:40 +0800

    feat: add category store & some request

---
+++ src/stores/categoryStore.ts
@@ -0,0 +1,37 @@
+import { ICategories, ICategory } from '@web/interfaces/ICategory'
+import { httpGet } from '@web/utils/axios'
+import { defineStore } from 'pinia'
+
+interface IState {
+  categoryList: ICategory[]
+  categoryData: ICategory
+}
+
+export const useCategoryStore = defineStore('category', {
+  state: (): IState => ({
+    categoryList: [],
+    categoryData: {},
+  }),
+
+  getters: {},
+
+  actions: {
+    async getCategories() {
+      try {
+        const result = <ICategories>await httpGet({ url: `/server/category/all` })
+        this.categoryList = result.categories
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+    },
+
+    async getCategoryById(id: string) {
+      try {
+        const result = <ICategory>await httpGet({ url: `/server/category/${id}` })
+        this.categoryData = result
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+    },
+  },
+})
