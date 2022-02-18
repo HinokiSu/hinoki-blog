083ee1471a08dcb970238fad019b0525c959bb8e
Hinoki <yukihinoki@qq.com>
2022-02-18 23:29:50 +0800

    feat: add separator component

---
+++ src/components/separator/separate-line.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hinoki__separator separate-line">
+    <div class="dashed-line"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SeparatorLine',
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.hinoki__separator {
+  width: inherit;
+  box-sizing: border-box;
+  display: block;
+  &.separate-line {
+    .dashed-line {
+      width: 100%;
+      align-self: center;
+      margin: 10px 0;
+
+      // ref: https://segmentfault.com/q/1010000002442556
+      height: 1px;
+      background-image: linear-gradient(to right, #ccc 0%, #ccc 50%, transparent 50%);
+      background-size: 8px 1px;
+      background-repeat: repeat-x;
+    }
+  }
+}
+</style>
