100a5a0fcc90ce072dfb55c6a5ad3b2332237f5e
Hinoki <yukihinoki@qq.com>
2022-02-18 23:36:00 +0800

    feat: add articles component

---
+++ src/views/article/articles.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>articles</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Articles',
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style scoped></style>
