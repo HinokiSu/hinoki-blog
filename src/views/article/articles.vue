7e9ccf8bde779e065ad7b2963375c1c251ce966a
Hinoki <yukihinoki@qq.com>
2022-02-19 23:10:13 +0800

    feat: update articles component

--- src/views/article/articles.vue
+++ src/views/article/articles.vue
@@ -1,16 +1,55 @@
 <template>
-  <div>articles</div>
+  <div class="hinoki-articles">
+    <div class="page-header">
+      <span class="header-title">Articles</span>
+    </div>
+    <separate-line></separate-line>
+    <div class="page-main">
+      <article-item class="article" :article="article" v-for="article in articles" :key="article._id"></article-item>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
+import { defineComponent, ref, watchEffect } from 'vue'
+import separateLine from '@web/components/separator/separate-line.vue'
+import articleItem from '@web/components/article-item/article-item.vue'
+import { useArticleStore } from '@web/stores/articleStore'
+import { IArticle } from '@web/interfaces/IArticle'
 export default defineComponent({
+  components: { separateLine, articleItem },
   name: 'Articles',
   setup() {
-    return {}
+    const ArticleStore = useArticleStore()
+    const articles = ref<IArticle[]>([])
+
+    watchEffect(() => {
+      if (ArticleStore.artilceCount === 0) {
+        ArticleStore.getAllArticle()
+        console.log('when length is 0 , Call this')
+      }
+      articles.value = ArticleStore.articleList
+    })
+    return {
+      articles,
+    }
   },
 })
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.hinoki-articles {
+  & .page-header {
+    .header-title {
+      font-size: 2rem;
+      font-weight: 500;
+    }
+  }
+
+  & .page-main {
+    display: flex;
+    flex-direction: column;
+    row-gap: 12px;
+  }
+}
+</style>
