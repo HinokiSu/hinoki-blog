b08e2af317533b8d833d0d6f7bd1c12cc042260d
Hinoki <yukihinoki@qq.com>
2022-03-09 11:22:34 +0800

    chore: add fect-ui themes

--- src/utils/third-ui/fect-ui.ts
+++ src/utils/third-ui/fect-ui.ts
@@ -3,7 +3,7 @@ import { GridGroup, Grid, User, Badge, Link, Avatar, Card, Image, Tag, Drawer } 
 Image.name = 'feImg'
 
 import '@fect-ui/vue/lib/main.css'
-
+import '@fect-ui/themes'
 const UI = [GridGroup, Grid, User, Badge, Link, Avatar, Card, Image, Tag, Drawer]
 
 export const FectUIRegister = (instance: any) => UI.map((_) => instance.use(_))
