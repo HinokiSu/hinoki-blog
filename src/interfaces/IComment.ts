3f0c0a2e7c8906b5c85bcabd0e02bf227fde58d7
Hinoki <yukihinoki@qq.com>
2022-04-13 22:46:45 +0800

    feat: add comment store&interface

---
+++ src/interfaces/IComment.ts
@@ -0,0 +1,41 @@
+import { IVisitor } from './IVisitor'
+
+export type IComment = {
+  _id: string
+  visitor: IVisitor
+  content: string
+  article_id: string
+  createdAt: string
+  child_comments: IChildComment[]
+}
+
+export type IParentComment = {
+  article_id: string
+  visitor_id: string
+  content: string
+  createdAt: string
+}
+
+export type IChildComment = {
+  visitor: IVisitor
+  content: string
+  reply_to_visitor: string
+  createdAt: string
+}
+
+export type IHttpComment = {
+  message: string
+  comments: IComment[]
+}
+
+export type IHttpCommentTotal = {
+  message: string
+  total: number
+}
+
+export type IReplyComment = {
+  visitor_id?: string
+  reply_to_visitor?: string
+  content?: string
+  createdAt?: string
+}
---
+++ src/stores/commentStore.ts
@@ -0,0 +1,75 @@
+import { IComment, IHttpComment, IHttpCommentTotal, IReplyComment } from '@web/interfaces/IComment'
+import { IParentComment } from '@web/interfaces/IComment'
+import { httpGet, httpPost } from '@web/utils/axios'
+import { defineStore } from 'pinia'
+
+interface IState {
+  commentList: IComment[]
+  commentTotal: number
+  replyComment: {
+    visitorId?: string
+    replyToNickname?: string
+  }
+  replyCommentId: string
+}
+
+export const useCommentStore = defineStore('comment', {
+  state: (): IState => ({
+    commentList: [],
+    commentTotal: 0,
+    replyComment: {},
+    replyCommentId: '',
+  }),
+
+  getters: {},
+
+  actions: {
+    // 根据文章id获取所有评论
+    async getCommentsByArticleId(articleId: string) {
+      try {
+        const res = <IHttpComment>await httpGet({ url: `/comment/${articleId}` })
+        this.commentList = res.comments
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+    },
+
+    // 根据文章id获取评论数（父子级）
+    async getCommentTotalByArticleId(articleId: string) {
+      try {
+        const res = <IHttpCommentTotal>await httpGet({ url: `/comment/count/${articleId}` })
+        this.commentTotal = res.total
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+    },
+
+    // 添加父级评论
+    async addCommentByArticleId(comment: IParentComment) {
+      try {
+        console.log('comment', comment)
+        const res = <IHttpComment>await httpPost({
+          url: `/comment/add`,
+          data: comment,
+        })
+
+        console.log(res)
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+    },
+
+    // 回复评论
+    async replyCommentByArticleId(comId: string, replyCommet: IReplyComment) {
+      try {
+        const res = await httpPost({
+          url: `/comment/reply/${comId}`,
+          data: replyCommet,
+        })
+        console.log('reply', res)
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+    },
+  },
+})
