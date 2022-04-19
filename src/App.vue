5fec07ac808edd02cc5921a271a741ab64244373
Hinoki <yukihinoki@qq.com>
2022-04-19 22:26:43 +0800

    feat: add transition feature

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default defineComponent({
 })
 </script>
 
-<style>
+<style lang="less">
 @import url('@web/assets/css/global.css');
 @import url('@web/assets/css/markdown-global.css');
 .fade-enter-active,
@@ -32,5 +32,42 @@ export default defineComponent({
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+
+.full-modal {
+  width: 50%;
+  margin: auto;
+
+  &::-webkit-scrollbar {
+    width: 0;
+  }
+  @media screen and (max-width: 700px) {
+    & {
+      width: 100%;
+      .ant-modal {
+        max-width: 100%;
+        top: 0;
+        padding-bottom: 0;
+        margin: 0;
+      }
+      .ant-modal-content {
+        display: flex;
+        flex-direction: column;
+        height: calc(100vh);
+      }
+      .ant-modal-body {
+        flex: 1;
+        
+      }
+    }
+  }
+
+  @media screen and (max-height: 400px) {
+    & {
+      & .ant-modal-content {
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
