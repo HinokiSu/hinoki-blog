7268ca068537d1b8797043dd7ba1898b75997fb1
Hinoki <yukihinoki@qq.com>
2022-04-19 22:19:06 +0800

    chore: modify article item&add tag icon

--- src/components/article-item/article-item.vue
+++ src/components/article-item/article-item.vue
@@ -1,27 +1,24 @@
 <template>
   <div class="article-item wrapper">
     <div class="item-container">
-      <div class="time">
-        <div class="day">{{ publishTime.day }}</div>
-        <div class="month">{{ publishTime.month }}月</div>
-      </div>
-      <div class="article-body">
-        <div class="article-title">
-          {{ article?.title }}
-        </div>
-        <div class="article-main">
-          <div class="category-tabs">
-            <fe-tag :text="tag.name" v-for="tag in article?.classification" :key="tag._id" type="success" use-invert />
+      <router-link :to="{ name: 'article-detail', params: { id: article?._id } }">
+        <div class="article-body">
+          <div class="article-caption">
+            <div class="article-title">{{ article?.title }}</div>
+            <separate-dot></separate-dot>
+            <div class="article-time">
+              {{ publishTime }}
+            </div>
           </div>
-          <div class="article-description">
-            {{ article?.description }}
+          <div class="article-main">
+            <div class="article-description">
+              {{ article?.description }}
+            </div>
           </div>
         </div>
-        <div class="article-footer">
-          <router-link :to="{ name: 'article-detail', params: { id: article?._id } }">
-            <div class="read-more">Read More</div>
-          </router-link>
-        </div>
+      </router-link>
+      <div class="article-footer" v-if="hasTags">
+        <article-tags :tags="article?.classification"></article-tags>
       </div>
     </div>
   </div>
@@ -29,21 +26,28 @@
 
 <script lang="ts">
 import { IArticle } from '@web/interfaces/IArticle'
-import { timeHasMonthDay } from '@web/utils/formatter'
+import { timeFormatToCNTime } from '@web/utils/formatter'
 import { computed, defineComponent, PropType, ref, watchEffect } from 'vue'
+import articleTags from './article-tags.vue'
+import SeparateDot from '../separator/separate-dot.vue'
 
 export default defineComponent({
   name: 'ArticleItem',
+  components: { articleTags, SeparateDot },
   props: {
     article: {
       type: Object as PropType<IArticle>,
     },
+    hasTags: {
+      type: Boolean,
+      default: true,
+    },
   },
   setup(props) {
-    const time = computed(() => <string>props.article?.updatedAt)
+    const time = computed(() => <string>props.article?.createdAt)
     const publishTime = ref()
     watchEffect(() => {
-      publishTime.value = timeHasMonthDay(time.value)
+      publishTime.value = timeFormatToCNTime(time.value)
     })
 
     return {
@@ -56,78 +60,55 @@ export default defineComponent({
 <style lang="less" scoped>
 .article-item {
   &.wrapper {
-    width: 100%;
-    box-shadow: 0 0 36px var(--accents-2);
-    border-radius: 24px;
+    padding-top: 24px;
     transition: all 0.4s ease;
-
-    &:hover {
-      box-shadow: 0 0 12px var(--accents-2);
-    }
+    border-bottom: 2px solid var(--accents-2);
   }
 
   & .item-container {
-    padding: 12px 8px;
-    display: grid;
-    grid-template-columns: 2fr 8fr;
+    padding: 8px 24px;
 
-    .time {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      row-gap: 4px;
-      font-weight: 500;
-
-      .day {
-        font-size: 2.3rem;
-        color: var(--success-default);
-      }
-      .month {
-        font-size: 1.5rem;
-        color: var(--highlight-pink);
-      }
-
-      @media screen and (max-width: 500px) {
-        .day {
-          font-size: 2em;
-        }
-
-        .month {
-          font-size: 1.2em;
-        }
+    @media screen and (max-width: 500px) {
+      & {
+        padding: 8px 12px;
       }
     }
 
     .article-body {
       display: flex;
       flex-direction: column;
-      row-gap: 4px;
       padding-right: 8px;
 
-      .article-title {
-        font-size: 1.2rem;
-        font-weight: 500;
-        color: var(--accents-6);
-        margin-bottom: 4px;
+      & .article-caption {
+        display: flex;
+        align-items: center;
+        padding-bottom: 8px;
 
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: normal;
-        -webkit-line-clamp: 1;
-        -webkit-box-orient: vertical;
-        word-break: break-all;
-        display: -webkit-box;
+        .article-title {
+          max-width: 60%;
+          font-size: 1.2rem;
+          font-weight: 500;
+          color: var(--accents-6);
+          margin-bottom: 4px;
+
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: normal;
+          -webkit-line-clamp: 1;
+          -webkit-box-orient: vertical;
+          word-break: break-all;
+          display: -webkit-box;
+        }
+
+        .article-time {
+          font-size: 0.8rem;
+          color: var(--accents-5);
+        }
       }
 
       .article-main {
         display: flex;
         flex-direction: column;
-        row-gap: 8px;
-        .category-tabs {
-          display: flex;
-          column-gap: 8px;
-        }
 
         .article-description {
           font-size: 1rem;
@@ -138,6 +119,8 @@ export default defineComponent({
           -webkit-box-orient: vertical;
           word-break: break-all;
           display: -webkit-box;
+
+          margin-bottom: 12px;
         }
 
         @media screen and (max-width: 500px) {
@@ -149,27 +132,9 @@ export default defineComponent({
       }
 
       .article-footer {
-        margin-top: 8px;
         align-self: flex-end;
-
-        .read-more {
-          padding: 8px 12px;
-          background-color: var(--success-default);
-          color: var(--accents-1);
-          font-size: 1rem;
-          font-weight: 600;
-          border-radius: 16px;
-          transition: all 0.4s ease;
-
-          &:hover {
-            background-color: var(--success-dark);
-          }
-        }
-        @media screen and (max-width: 500px) {
-          & .read-more {
-            font-size: 0.8rem;
-          }
-        }
+        display: flex;
+        flex-direction: row;
       }
     }
   }
