b78b342effbe7eb9a8a41f5d510327497b84a9ec
Hinoki <yukihinoki@qq.com>
2022-04-19 22:25:22 +0800

    feat: add method of get real time

--- src/utils/formatter/formatDate.ts
+++ src/utils/formatter/formatDate.ts
@@ -10,3 +10,25 @@ export const timeFormatToCNTime = (raw: string): string => {
   formatedTime = `${time[0]}年${time[1]}月${time[2]}日`
   return formatedTime
 }
+
+import dayjs from 'dayjs'
+import { Ref } from 'vue'
+
+/**
+ * 获取实时时间
+ * @param val Proxy 实时的时间
+ * @param delay 实时时间的更新间隔
+ * @param format 时间的格式,default:'YYYY-MM-DD hh:mm:ss'
+ * @returns intervalId，用于清除定时器
+ */
+export function getRealTime(val: Ref, delay: number, format: string = 'YYYY-MM-DD HH:mm') {
+  return setInterval(
+    (val: { value: string }, format: string) => {
+      const time = dayjs().format(format)
+      val.value = `${timeFormatToCNTime(time)} ${time.split(' ')[1]}`
+    },
+    delay,
+    val,
+    format
+  )
+}
