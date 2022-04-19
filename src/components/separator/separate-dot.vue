3f97f7c9609b7c45ab203c5d9373c46c667439fb
Hinoki <yukihinoki@qq.com>
2022-04-19 22:08:10 +0800

    feat: add spearate dot component

---
+++ src/components/separator/separate-dot.vue
@@ -0,0 +1,41 @@
+<template>
+  <span class="sperator-dot" :style="styles"></span>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SeparateDot',
+  props: {
+    w: {
+      type: String,
+      default: '4px',
+    },
+    h: {
+      type: String,
+      default: '4px',
+    },
+    color: {
+      type: String,
+      default: '#888',
+    },
+  },
+  setup(props) {
+    return {
+      styles: computed(() => ({
+        width: props.w,
+        height: props.h,
+        'background-color': props.color,
+      })),
+    }
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.sperator-dot {
+  margin: 0 4px;
+  border-radius: 50%;
+}
+</style>
