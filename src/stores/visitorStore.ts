48a5d07f190c26e8cf5a4aea785801300148a9b2
Hinoki <yukihinoki@qq.com>
2022-04-19 22:26:09 +0800

    chore: modify visitor login method

--- src/stores/visitorStore.ts
+++ src/stores/visitorStore.ts
@@ -1,17 +1,36 @@
-import { ILoginVisitor, IRegisterVisitor, IVisitor } from '@web/interfaces/IVisitor'
+import { IHttpVisitor, ILoginVisitor, IRegisterVisitor, IVisitor } from '@web/interfaces/IVisitor'
 import { httpGet, httpPost } from '@web/utils/axios'
 import { defineStore } from 'pinia'
 
 interface IState {
   visitorData: IVisitor
+  visitorStatus: boolean
 }
 
 export const useVisitorStore = defineStore('visitor', {
   state: (): IState => ({
     visitorData: {},
+    visitorStatus: false,
   }),
-  getters: {},
+  getters: {
+    getVisitor: (state) => {
+      if (Object.keys(state.visitorData).length === 0) {
+        state.visitorData = JSON.parse(localStorage.getItem('visitor') || '{}')
+      }
+      return state.visitorData
+    },
+    getLoginStatus: (state) => {
+      if (Object.keys(state.visitorData).length !== 0) {
+        state.visitorStatus = true
+      }
+      return state.visitorStatus
+    },
+  },
   actions: {
+    // 获取本地
+    getLocalVisitor() {
+      this.visitorData = JSON.parse(localStorage.getItem('visitor') || '{}')
+    },
     // 注册 visitor
     async registerVisitor(visitor: IRegisterVisitor) {
       try {
@@ -27,16 +46,14 @@ export const useVisitorStore = defineStore('visitor', {
 
     // 登录 visitor
     async loginVisitor(visitor: ILoginVisitor) {
-      try {
-        const res = await httpPost({
-          url: `/visitor/login`,
-          data: visitor,
-        })
+      const res = <IHttpVisitor>await httpPost({
+        url: `/visitor/login`,
+        data: visitor,
+      })
 
-        console.log('login', res)
-      } catch (error) {
-        console.log(`Error: ${error}`)
-      }
+      // 存储到 localStorage
+      localStorage.setItem('visitor', JSON.stringify(res.visitor))
+      this.visitorData = res.visitor
     },
   },
 })
