04a933686a1c8892a381391d0c944d7784dfbce2
Hinoki <yukihinoki@qq.com>
2022-05-27 17:17:06 +0800

    chore: modify content of about component

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="hinoki-blog about">
     <content-layout header-title="关于">
-      <div class="about__introduction"></div>
+      <div class="about__introduction">
+        <p>你好!👋</p>
+        <p>我是 Hinoki , 目前我从事于 前端开发 和 RPA开发的工作。热衷于使用 Vue 和 TypeScript进行开发。</p>
+        <p>若对本博客的内容有新的想法🤔, 欢迎你在评论中表达出来✨。</p>
+      </div>
     </content-layout>
   </div>
 </template>
@@ -21,7 +25,12 @@ export default defineComponent({
 <style lang="less" scoped>
 .hinoki-blog {
   &.about {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
     .about__introduction {
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      font-size: 1.15rem;
     }
   }
 }
